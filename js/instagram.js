e8581106ca9b20b43c10fea2fc85e2dd6df34118
Jason Pu <puj@Jasons-MacBook-Pro.local>
2016-12-27 20:59:12 -0800

    new insta module

--- js/instagram.js
+++ js/instagram.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function() {
       userId: '294138773',
       tagName: 'awesome',
       clientId: 'a009d9ab303449348234ff0b47d6b3c9',
-      accessToken: '294138773.ba4c844.e96b32e327834ee5ae206a2ea7a28834',
+      accessToken: '294138773.a009d9a.14f8f389b0bc41cb8823c940fdd04b0e',
       sortBy: 'most-recent',
       limit: '100',
       resolution: 'low_resolution'
